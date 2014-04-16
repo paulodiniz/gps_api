9265633b5bc04500b63e4d744ca173fae084b281
Paulo Diniz <paulo.rc.diniz@gmail.com>
2014-04-16 03:43:34 +0000

    using mongo id spacial

--- bus.rb
+++ bus.rb
@@ -1,8 +1,15 @@
 require 'mongoid'
+require 'mongoid_spacial'
 
 class Bus
   include Mongoid::Document
+  include Mongoid::Spacial::Document
+
   field :name, type: String
   field :line, type: String
   field :code, type: String
+
+  field :position,            type: Array,    spacial: true
+
+  # spacial_index :position
 end
--- gps_api.rb
+++ gps_api.rb
@@ -9,6 +9,10 @@ class GpsApi < Sinatra::Base
 
   post '/line/:line_id/bus/:bus_code/location' do
     b = Bus.where(line: params[:line_id], code: params[:bus_code]).first_or_create
+    if params[:lat] && params[:lng]
+      b.position = {:lat => params[:lat].to_f, :lng => params[:lng].to_f}
+      b.save
+    end
     b.to_json
   end
 
